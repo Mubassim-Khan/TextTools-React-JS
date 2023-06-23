c464aac44ae80a18777506b830175d309321d9ab
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2023-06-23 21:26:16 +0500

    #8 Completed - Added & improved some new features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import propTypes from "prop-types";
 
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
-export default function (props) {
-    const [text, setText] = useState("Enter your text");        // useState hook
+export default function TextBox(props) {
+    const [text, setText] = useState("");        // useState hook
 
     const handleChanging = (event) => {             // Event Listener/handler for the Text Area
         // console.log("Handle Changed");
@@ -20,14 +20,26 @@ export default function (props) {
         setText(newText);
     }
 
+    let words = text.split(" ").length - 1;
+
     return (
-        <div>
+        <>
+        <div className='container'>
             <h1>{props.textBoxName}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} id="textBox" rows="8" onChange={handleChanging}></textarea>
+                <textarea className="form-control my-3" value={text} id="textBox" rows="8" onChange={handleChanging}></textarea>
             </div>
-            <div className="btn btn-primary" onClick={convertUpperCase}>Convert into UpperCase</div>
-            <div className="btn btn-primary mx-3" onClick={convertLowerCase}>Convert into LowerCase</div>
+            <div className="btn btn-outline-primary" onClick={convertUpperCase}>Convert into UpperCase</div>
+            <div className="btn btn-outline-primary mx-3" onClick={convertLowerCase}>Convert into LowerCase</div>
         </div>
+        {/* Counting Words, characters & preview the written text */}
+        <div className="container my-3">
+            <p>Total Words used: <b>{words}</b></p>
+            <p>Total Characters used: <b>{text.length}</b></p>
+            <p>Total time took to read: <b>{Math.floor(0.08*words)}</b> minutes</p>
+            <h3>Preview of your text</h3>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
